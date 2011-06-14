e618043e96d3488ff9e0d1fd4f1656a097b3b699
Rohan Drape <rd@slavepianos.org>
2011-06-14 10:25:39 +0000

    Coerce (reading)

--- Sound/OpenSoundControl/Coerce.hs
+++ Sound/OpenSoundControl/Coerce.hs
@@ -5,25 +5,33 @@ import Sound.OpenSoundControl.OSC.Type
 
 -- | Map a normalizing function over datum at an osc packet.
 coerce :: (Datum -> Datum) -> OSC -> OSC
-coerce f (Message s xs) = Message s (map f xs)
-coerce f (Bundle t xs) = Bundle t (map (coerce f) xs)
+coerce f o =
+    case o of
+      Message s xs -> Message s (map f xs)
+      Bundle t xs -> Bundle t (map (coerce f) xs)
 
 -- | Coerce Float to Double.
 f_to_d :: Datum -> Datum
-f_to_d (Float n) = Double n
-f_to_d x = x
+f_to_d d =
+    case d of
+      Float n -> Double n
+      _ -> d
 
 -- | Coerce Int and Float to Double.
 if_to_d :: Datum -> Datum
-if_to_d (Int n) = Double (fromIntegral n)
-if_to_d (Float n) = Double n
-if_to_d x = x
+if_to_d d =
+    case d of
+      Int n -> Double (fromIntegral n)
+      Float n -> Double n
+      _ -> d
 
 -- | Coerce Float and Double to Int.
 fd_to_i :: Datum -> Datum
-fd_to_i (Float n) = Int (round n)
-fd_to_i (Double n) = Int (round n)
-fd_to_i x = x
+fd_to_i d =
+    case d of
+      Float n -> Int (round n)
+      Double n -> Int (round n)
+      _ -> d
 
 -- | A normalized osc packet has only Int and Double numerical values.
 normalize :: OSC -> OSC
