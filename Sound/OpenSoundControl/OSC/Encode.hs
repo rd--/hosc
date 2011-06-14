ad3e32a90e61489c18a585b140a45093e6c27194
Rohan Drape <rd@slavepianos.org>
2011-06-14 02:22:10 +0000

    OSC.Encode (reading)

--- Sound/OpenSoundControl/OSC/Encode.hs
+++ Sound/OpenSoundControl/OSC/Encode.hs
@@ -1,6 +1,5 @@
--- | Alegbraic data types for OSC packets and encode and decode
---   functions.
-module Sound.OpenSoundControl.OSC.Encode ( encodeOSC ) where
+-- | Encode function for OSC packets.
+module Sound.OpenSoundControl.OSC.Encode (encodeOSC) where
 
 import qualified Data.ByteString.Lazy as B
 import Data.Word
@@ -18,20 +17,23 @@ extend p s = B.append s (B.replicate (align (B.length s)) p)
 
 -- Encode an OSC datum.
 encode_datum :: Datum -> B.ByteString
-encode_datum (Int i) = encode_i32 i
-encode_datum (Float f) = encode_f32 f
-encode_datum (Double d) = encode_f64 d
-encode_datum (TimeStamp t) = encode_u64 $ as_ntpi t
-encode_datum (String s) = extend 0 (B.snoc (encode_str s) 0)
-encode_datum (Midi (b0,b1,b2,b3)) = B.pack [b0,b1,b2,b3]
-encode_datum (Blob b) = B.append (encode_i32 (fromIntegral (B.length b))) (extend 0 b)
+encode_datum dt =
+    case dt of
+      Int i -> encode_i32 i
+      Float f -> encode_f32 f
+      Double d -> encode_f64 d
+      TimeStamp t -> encode_u64 $ as_ntpi t
+      String s -> extend 0 (B.snoc (encode_str s) 0)
+      Midi (b0,b1,b2,b3) -> B.pack [b0,b1,b2,b3]
+      Blob b -> let n = encode_i32 (fromIntegral (B.length b))
+                in B.append n (extend 0 b)
 
 -- Encode an OSC message.
 encode_message :: String -> [Datum] -> B.ByteString
-encode_message c l = 
-    B.concat [ encode_datum (String c)
-             , encode_datum (descriptor l)
-             , B.concat (map encode_datum l) ]
+encode_message c l =
+    B.concat [encode_datum (String c)
+             ,encode_datum (descriptor l)
+             ,B.concat (map encode_datum l) ]
 
 -- Encode an OSC packet as an OSC blob.
 encode_osc_blob :: OSC -> Datum
@@ -40,13 +42,15 @@ encode_osc_blob = Blob . encodeOSC
 -- Encode an OSC bundle.
 encode_bundle_ntpi :: NTPi -> [OSC] -> B.ByteString
 encode_bundle_ntpi t l =
-    B.concat [ bundleHeader
-             , encode_u64 t
-             , B.concat (map (encode_datum . encode_osc_blob) l) ]
+    B.concat [bundleHeader
+             ,encode_u64 t
+             ,B.concat (map (encode_datum . encode_osc_blob) l) ]
 
 -- | Encode an OSC packet.
 encodeOSC :: OSC -> B.ByteString
-encodeOSC (Message c l) = encode_message c l
-encodeOSC (Bundle (NTPi t) l) = encode_bundle_ntpi t l
-encodeOSC (Bundle (NTPr t) l) = encode_bundle_ntpi (ntpr_ntpi t) l
-encodeOSC (Bundle (UTCr t) l) = encode_bundle_ntpi (utcr_ntpi t) l
+encodeOSC o =
+    case o of
+      Message c l -> encode_message c l
+      Bundle (NTPi t) l -> encode_bundle_ntpi t l
+      Bundle (NTPr t) l -> encode_bundle_ntpi (ntpr_ntpi t) l
+      Bundle (UTCr t) l -> encode_bundle_ntpi (utcr_ntpi t) l
