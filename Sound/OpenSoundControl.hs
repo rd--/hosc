159254250cd8137a4f478ec21ebd6c2957f036c9
Rohan Drape <rd@slavepianos.org>
2011-11-21 22:13:07 +0000

    haddock (top level clarifications)

--- Sound/OpenSoundControl.hs
+++ Sound/OpenSoundControl.hs
@@ -1,10 +1,10 @@
--- | @hosc@ implements a subset of the /Open Sound Control/ byte
+-- | An implementation of a subset of the /Open Sound Control/ byte
 -- protocol, documented at <http://opensoundcontrol.org/>.
 --
 -- For the most part this top-level module is the only import
--- required.  It provides the 'Datum' and 'OSC' types, encoding and
--- decoding functions, basic 'UDP' and 'TCP' 'Transport' layers, and
--- basic temporal operations 'utcr' to access the current time and
+-- required.  It provides the 'Datum' and 'OSC' types, 'encodeOSC' and
+-- 'decodeOSC' functions, basic 'UDP' and 'TCP' 'Transport' layers,
+-- and basic temporal operations 'utcr' to access the current time and
 -- 'pauseThread' to delay the current thread.
 --
 -- > let o = Bundle immediately [Message "/g_free" [Int 0]]
