6b2514336792dd1f7b1399448eaceec625a33020
Rohan Drape <rd@slavepianos.org>
2012-11-26 23:32:46 +0000

    +RecvOSC,revert Transport

--- Sound/OSC/Transport/Monad.hs
+++ Sound/OSC/Transport/Monad.hs
@@ -11,19 +11,27 @@ import qualified Sound.OSC.Transport.FD as T
 import Sound.OpenSoundControl.Type
 import Sound.OpenSoundControl.Wait
 
+-- | Sender monad.
 class Monad m => SendOSC m where
    -- | Encode and send an OSC packet.
    sendOSC :: OSC o => o -> m ()
 
-class SendOSC m => Transport m where
+-- | Receiver monad.
+class Monad m => RecvOSC m where
    -- | Receive and decode an OSC packet.
    recvPacket :: m Packet
+
+-- | 'Transport' is the union of 'SendOSC' and 'RecvOSC' and
+-- incorporates a 'MonadIO' constraint.
+class (SendOSC m,RecvOSC m,MonadIO m) => Transport m where
 
 instance (T.Transport t,MonadIO io) => SendOSC (ReaderT t io) where
    sendOSC o = ReaderT (M.liftIO . flip T.sendOSC o)
 
-instance (T.Transport t,MonadIO io) => Transport (ReaderT t io) where
+instance (T.Transport t,MonadIO io) => RecvOSC (ReaderT t io) where
    recvPacket = ReaderT (M.liftIO . T.recvPacket)
+
+instance (T.Transport t,MonadIO io) => Transport (ReaderT t io) where
 
 -- | Transport connection.
 type Connection t a = ReaderT t IO a
@@ -45,51 +53,51 @@ sendBundle = sendOSC
 -- * Receive
 
 -- | Variant of 'recvPacket' that runs 'fromPacket'.
-recvOSC :: (Transport m,OSC o) => m (Maybe o)
+recvOSC :: (RecvOSC m,OSC o) => m (Maybe o)
 recvOSC = liftM fromPacket recvPacket
 
 -- | Variant of 'recvPacket' that runs 'packet_to_bundle'.
-recvBundle :: (Transport m) => m Bundle
+recvBundle :: (RecvOSC m) => m Bundle
 recvBundle = liftM packet_to_bundle recvPacket
 
 -- | Variant of 'recvPacket' that runs 'packet_to_message'.
-recvMessage :: (Transport m) => m (Maybe Message)
+recvMessage :: (RecvOSC m) => m (Maybe Message)
 recvMessage = liftM packet_to_message recvPacket
 
 -- | Variant of 'recvPacket' that runs 'packetMessages'.
-recvMessages :: (Transport m) => m [Message]
+recvMessages :: (RecvOSC m) => m [Message]
 recvMessages = liftM packetMessages recvPacket
 
 -- * Wait
 
 -- | Wait for a 'Packet' where the supplied predicate is 'True',
 -- discarding intervening packets.
-waitUntil :: (Transport m) => (Packet -> Bool) -> m Packet
+waitUntil :: (RecvOSC m) => (Packet -> Bool) -> m Packet
 waitUntil f = untilPredicate f recvPacket
 
 -- | Wait for a 'Packet' where the supplied function does not give
 -- 'Nothing', discarding intervening packets.
-waitFor :: (Transport m) => (Packet -> Maybe a) -> m a
+waitFor :: (RecvOSC m) => (Packet -> Maybe a) -> m a
 waitFor f = untilMaybe f recvPacket
 
 -- | 'waitUntil' 'packet_is_immediate'.
-waitImmediate :: Transport m => m Packet
+waitImmediate :: RecvOSC m => m Packet
 waitImmediate = waitUntil packet_is_immediate
 
 -- | 'waitFor' 'packet_to_message', ie. an incoming 'Message' or
 -- immediate mode 'Bundle' with one element.
-waitMessage :: Transport m => m Message
+waitMessage :: RecvOSC m => m Message
 waitMessage = waitFor packet_to_message
 
 -- | A 'waitFor' for variant using 'packet_has_address' to match on
 -- the 'Address_Pattern' of incoming 'Packets'.
-waitAddress :: Transport m => Address_Pattern -> m Packet
+waitAddress :: RecvOSC m => Address_Pattern -> m Packet
 waitAddress s =
     let f o = if packet_has_address s o then Just o else Nothing
     in waitFor f
 
 -- | Variant on 'waitAddress' that returns matching 'Message'.
-waitReply :: Transport m => Address_Pattern -> m Message
+waitReply :: RecvOSC m => Address_Pattern -> m Message
 waitReply s =
     let f = fromMaybe (error "waitReply: message not located?") .
             find (message_has_address s) .
@@ -97,5 +105,5 @@ waitReply s =
     in liftM f (waitAddress s)
 
 -- | Variant of 'waitReply' that runs 'messageDatum'.
-waitDatum :: Transport m => Address_Pattern -> m [Datum]
+waitDatum :: RecvOSC m => Address_Pattern -> m [Datum]
 waitDatum = liftM messageDatum . waitReply
