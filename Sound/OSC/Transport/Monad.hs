e03481b6269534e47264b154fb63c59203664bc1
Stefan Kersten <sk@xdv.org>
2012-11-26 13:05:24 +0000

    Split Transport class into SendOSC and Transport

--- Sound/OSC/Transport/Monad.hs
+++ Sound/OSC/Transport/Monad.hs
@@ -1,6 +1,7 @@
 -- | Monad class implementing an Open Sound Control transport.
 module Sound.OSC.Transport.Monad where
 
+import Control.Monad (liftM)
 import Control.Monad.Trans.Reader {- transformers -}
 import Control.Monad.IO.Class as M
 import Data.List
@@ -10,14 +11,18 @@ import qualified Sound.OSC.Transport.FD as T
 import Sound.OpenSoundControl.Type
 import Sound.OpenSoundControl.Wait
 
-class (Functor m,Monad m,MonadIO m) => Transport m where
+class Monad m => SendOSC m where
    -- | Encode and send an OSC packet.
    sendOSC :: OSC o => o -> m ()
+
+class SendOSC m => Transport m where
    -- | Receive and decode an OSC packet.
    recvPacket :: m Packet
 
-instance (T.Transport t,Functor io,MonadIO io) => Transport (ReaderT t io) where
+instance (T.Transport t,MonadIO io) => SendOSC (ReaderT t io) where
    sendOSC o = ReaderT (M.liftIO . flip T.sendOSC o)
+
+instance (T.Transport t,MonadIO io) => Transport (ReaderT t io) where
    recvPacket = ReaderT (M.liftIO . T.recvPacket)
 
 -- | Transport connection.
@@ -30,30 +35,30 @@ withTransport u = T.withTransport u . runReaderT
 -- * Send
 
 -- | Type restricted synonym for 'sendOSC'.
-sendMessage :: Transport m => Message -> m ()
+sendMessage :: SendOSC m => Message -> m ()
 sendMessage = sendOSC
 
 -- | Type restricted synonym for 'sendOSC'.
-sendBundle :: Transport m => Bundle -> m ()
+sendBundle :: SendOSC m => Bundle -> m ()
 sendBundle = sendOSC
 
 -- * Receive
 
 -- | Variant of 'recvPacket' that runs 'fromPacket'.
 recvOSC :: (Transport m,OSC o) => m (Maybe o)
-recvOSC = fmap fromPacket recvPacket
+recvOSC = liftM fromPacket recvPacket
 
 -- | Variant of 'recvPacket' that runs 'packet_to_bundle'.
 recvBundle :: (Transport m) => m Bundle
-recvBundle = fmap packet_to_bundle recvPacket
+recvBundle = liftM packet_to_bundle recvPacket
 
 -- | Variant of 'recvPacket' that runs 'packet_to_message'.
 recvMessage :: (Transport m) => m (Maybe Message)
-recvMessage = fmap packet_to_message recvPacket
+recvMessage = liftM packet_to_message recvPacket
 
 -- | Variant of 'recvPacket' that runs 'packetMessages'.
 recvMessages :: (Transport m) => m [Message]
-recvMessages = fmap packetMessages recvPacket
+recvMessages = liftM packetMessages recvPacket
 
 -- * Wait
 
@@ -89,8 +94,8 @@ waitReply s =
     let f = fromMaybe (error "waitReply: message not located?") .
             find (message_has_address s) .
             packetMessages
-    in fmap f (waitAddress s)
+    in liftM f (waitAddress s)
 
 -- | Variant of 'waitReply' that runs 'messageDatum'.
 waitDatum :: Transport m => Address_Pattern -> m [Datum]
-waitDatum = fmap messageDatum . waitReply
+waitDatum = liftM messageDatum . waitReply
