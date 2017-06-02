1ec5b4a6bc4f81a8eefe8433aba2ca8f2d175072
Rohan Drape <rd@slavepianos.org>
2017-06-02 01:44:57 +0000

    +Sound/OSC/Unpack.hs

---
+++ Sound/OSC/Unpack.hs
@@ -0,0 +1,40 @@
+-- | Unpack 'Datum' lists into tuples.
+module Sound.OSC.Unpack where
+
+import Data.Int {- base -}
+
+import Sound.OSC.Type {- hosc -}
+
+-- * S = strict
+
+unpackS_sf :: [Datum] -> Maybe (String,Float)
+unpackS_sf dat =
+    case dat of
+      [ASCII_String d1,Float d2] -> Just (ascii_to_string d1,d2)
+      _ -> Nothing
+
+unpackS_if :: [Datum] -> Maybe (Int32,Float)
+unpackS_if dat =
+    case dat of
+      [Int32 d1,Float d2] -> Just (d1,d2)
+      _ -> Nothing
+
+-- * C = coerce
+
+unpackC_sf :: [Datum] -> Maybe (String,Double)
+unpackC_sf dat =
+    case dat of
+      [ASCII_String d1,d2] ->
+        case datum_floating d2 of
+          Just d2' -> Just (ascii_to_string d1,d2')
+          Nothing -> Nothing
+      _ -> Nothing
+
+unpackC_if :: [Datum] -> Maybe (Int,Double)
+unpackC_if dat =
+    case dat of
+      [d1,d2] ->
+          case (datum_integral d1,datum_floating d2) of
+            (Just d1',Just d2') -> Just (d1',d2')
+            _ -> Nothing
+      _ -> Nothing
