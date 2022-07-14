7a91b1d4bc2306787661d41f30fcb8f15b9903da
rohan <rd@slavepianos.org>
2022-07-15 06:05:30 +1000

    textisnotcore

--- Sound/Osc/Core.hs
+++ Sound/Osc/Core.hs
@@ -16,7 +16,6 @@ import Sound.Osc.Coding.Decode.Binary as M
 import Sound.Osc.Coding.Encode.Builder as M
 import Sound.Osc.Datum as M
 import Sound.Osc.Packet as M
-import Sound.Osc.Text as M
 import Sound.Osc.Time as M
 import Sound.Osc.Time.System as M
 import Sound.Osc.Time.Thread as M
