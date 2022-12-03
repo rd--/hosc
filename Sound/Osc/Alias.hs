96b56e25f2dcf02fd179e8cab6938a6742dce73e
rohan <rd@slavepianos.org>
2022-12-03 15:54:34 +1100

    -alias

--- Sound/Osc/Alias.hs
+++
@@ -1,9 +0,0 @@
--- | Some backwards compatability, not really...
-module Sound.Osc.Alias where
-
-import Sound.Osc.Datum {- hosc -}
-
-type Datum_Type = DatumType
-type BLOB = Blob
-type ASCII = Ascii
-type MIDI = MidiData
